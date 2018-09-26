c807c370e3472862f86f0a322423f5d7af5d9fc0
Andrey Kukuruza <andrii.kukuruza@inbase.com.ua>
2018-09-26 19:02:44 +0300

    read folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const parseChangelog = require('./parser.js')
-const changeTypes = ['Added', 'Changed', 'Deprecated', 'Removed', 'Fixed', 'Security']
+const fs = require('fs')
 
-const changes = { 'Added': [], 'Changed': [], 'Deprecated': [], 'Removed': [], 'Fixed': [], 'Security': [] }
-parseChangelog('CHANGELOG.md').then(json => {
-  const groupChanges = json.versions.reduce((prev, curr) => {
-    for (let changeType of changeTypes) {
-      if (curr.parsed[changeType]) {
-        prev[changeType] = [...prev[changeType], ...curr.parsed[changeType]]
-      }
+const dirs = fs.readdir('./packages').then(packages => {
+    for (let pkg of packages) {
+      //todo
     }
-    return prev
-  }, changes)
-})
+  }
+)
+
+const groupChanges = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const changeTypes = ['Added', 'Changed', 'Deprecated', 'Removed', 'Fixed', 'Security']
+    const changes = { 'Added': [], 'Changed': [], 'Deprecated': [], 'Removed': [], 'Fixed': [], 'Security': [] }
+    parseChangelog(filePath).then(json => {
+      const regroupChanges = json.versions.reduce((prev, curr) => {
+        for (let changeType of changeTypes) {
+          if (curr.parsed[changeType]) {
+            prev[changeType] = [...prev[changeType], ...curr.parsed[changeType]]
+          }
+        }
+        return prev
+      }, changes)
+      resolve(regroupChanges)
+    })
+  })
+}
